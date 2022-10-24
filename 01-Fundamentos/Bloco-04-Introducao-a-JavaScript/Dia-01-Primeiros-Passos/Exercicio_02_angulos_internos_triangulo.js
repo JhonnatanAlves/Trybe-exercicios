3e0257f44fea862d91624b7d9df1ee5fd20ad1c3
Jhon-Alves <jhonalves.jsa@icloud.com>
2022-10-23 22:35:25 -0300

    Adicionado exercicio dos angulos

---
+++ 01-Fundamentos/Bloco-04-Introducao-a-JavaScript/Dia-01-Primeiros-Passos/Exercicio_02_angulos_internos_triangulo.js
@@ -0,0 +1,12 @@
+const a = 40;
+const b = 50;
+const c = 90;
+const soma = a + b + c;
+
+if (soma === 180){
+    console.log(true);
+} else if (soma < 180){
+    console.log(false);
+} else {
+    console.log("Erro, angulo invalido");
+}
