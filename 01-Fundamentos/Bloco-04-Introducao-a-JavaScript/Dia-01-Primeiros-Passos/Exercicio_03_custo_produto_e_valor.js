0795052025d5b212e33681364c930f9804d736b6
Jhon-Alves <jhonalves.jsa@icloud.com>
2022-10-26 00:14:01 -0300

    Adicionado exercicio custo do produto

---
+++ 01-Fundamentos/Bloco-04-Introducao-a-JavaScript/Dia-01-Primeiros-Passos/Exercicio_03_custo_produto_e_valor.js
@@ -0,0 +1,12 @@
+
+const custoDoProduto = 200;
+const valorDeVenda = 400;
+
+if (custoDoProduto >= 0 && valorDeVenda >= 0) {
+  const custoTotalDoProduto = custoDoProduto * 1.2;
+  const totalDaVenda = (valorDeVenda - custoTotalDoProduto) * 1000;
+  console.log(totalDaVenda);
+} else {
+  console.log("Error, os valores não podem ser negativos");
+};
+
