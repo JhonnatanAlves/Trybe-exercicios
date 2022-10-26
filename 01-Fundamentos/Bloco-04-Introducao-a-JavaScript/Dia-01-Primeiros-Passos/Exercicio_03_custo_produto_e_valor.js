668d1131e2a22720cd55c49a682f6338f2ea40ba
Jhon-Alves <jhonalves.jsa@icloud.com>
2022-10-26 10:38:48 -0300

    Add

--- 01-Fundamentos/Bloco-04-Introducao-a-JavaScript/Dia-01-Primeiros-Passos/Exercicio_03_custo_produto_e_valor.js
+++ 01-Fundamentos/Bloco-04-Introducao-a-JavaScript/Dia-01-Primeiros-Passos/Exercicio_03_custo_produto_e_valor.js
@@ -3,10 +3,9 @@ const custoDoProduto = 200;
 const valorDeVenda = 400;
 
 if (custoDoProduto >= 0 && valorDeVenda >= 0) {
-  const custoTotalDoProduto = custoDoProduto * 1.2;
+  const custoTotalDoProduto = custoDoProduto * 0.20;
   const totalDaVenda = (valorDeVenda - custoTotalDoProduto) * 1000;
   console.log(totalDaVenda);
 } else {
   console.log("Error, os valores não podem ser negativos");
 };
-
